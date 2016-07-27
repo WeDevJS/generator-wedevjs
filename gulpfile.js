1bb1c2ac329928134ab7f46e796dc37dbbb62780
Yording Cala Sanchez <Yording Cala Sanchez>
2016-07-27 17:47:35 -0500

    add the task gulp for linter sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,66 @@
 'use strict';
 
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename');
-
+	gulpLoadPlugins=require('gulp-load-plugins'),
+	plugins = gulpLoadPlugins({
+	    lazy: true,
+	    rename:{
+	    	'gulp-sass':'sass',
+	    	'gulp-autoprefixer':'autoprefixer',
+	    	'gulp-minify-css':'minifycss',
+	    	'gulp-rename':'rename',
+	    	'gulp-sass-lint':'sassLint'
+	    }
+	});
 //constants paths
 const paths = {
     client: {
         css:'./css',
-        sass:'sass/**/*.scss'
+        sass:'sass/**/*.scss',
+    },
+    config:{
+    	sass_lint:'config/sass/*.sass-lint.yml'
     }
 }
 /**
  * Tasks
  */
 
-// process files sass to css
-// minify files
-// rename css
-gulp.task('styles',function(){
+// The first task is to verify that no errors styles
+gulp.task('styles',['styles:lint'],function(){
 	gulp.src(paths.client.sass) 
-	.pipe(sass().on('error',sass.logError))//sass to css, if it returns a compilation error stops execution
-	.pipe(autoprefixer({
+	.pipe(plugins.sass().on('error',plugins.sass.logError))//sass to css, if it returns a compilation error stops execution
+	.pipe(plugins.autoprefixer({ // autoprefixer
 		browsers: ['last 2 versions'],
         cascade: false
 	}))
 	.pipe(gulp.dest(paths.client.css)) //add to folder the files minify
-	.pipe(rename({suffix: '.min'})) //rename the css adding ".min"
-    .pipe(minifycss()) //minify css
+	.pipe(plugins.rename({suffix: '.min'})) //rename the css adding ".min"
+    .pipe(plugins.minifycss()) //minify css
     .pipe(gulp.dest(paths.client.css)); //add to folder the files minify 
 
 });
-//sub-task of 'styles'
+
+
 //observe Each time a file css is processed
 gulp.task('styles:watch',function(){
 	gulp.watch(paths.client.sass,['styles']);
 });
 
-//task default
-gulp.task('default',['styles:watch'],function(){
+//in the future to add more linter style tasks
+gulp.task('styles:lint', ['styles:lint:sass'], function(){});
 
+//special task for errors in compilation of linter styles sass
+gulp.task('styles:lint:sass',function(){
+	return gulp.src(paths.client.sass)
+    .pipe(plugins.sassLint({
+      config: paths.config.sass_lint
+    }))
+    .pipe(plugins.sassLint.format())
+    .pipe(plugins.sassLint.failOnError())
+})
+
+//task default
+gulp.task('default',['styles','styles:watch'],function(){
+	
 });
