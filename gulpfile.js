a4c032e0fc4473849dda26fb74db73a1a22672e4
Yording Cala Sanchez <Yording Cala Sanchez>
2016-07-27 12:28:57 -0500

    task sass minify,process,autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,45 @@
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')({
-    lazy: true
+'use strict';
+
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
+
+//constants paths
+const paths = {
+    client: {
+        css:'./css',
+        sass:'sass/**/*.scss'
+    }
+}
+/**
+ * Tasks
+ */
+
+// process files sass to css
+// minify files
+// rename css
+gulp.task('styles',function(){
+	gulp.src(paths.client.sass) 
+	.pipe(sass().on('error',sass.logError))//sass to css, if it returns a compilation error stops execution
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+        cascade: false
+	}))
+	.pipe(gulp.dest(paths.client.css)) //add to folder the files minify
+	.pipe(rename({suffix: '.min'})) //rename the css adding ".min"
+    .pipe(minifycss()) //minify css
+    .pipe(gulp.dest(paths.client.css)); //add to folder the files minify 
+
 });
-var config = require('./gulp.config')();
+//sub-task of 'styles'
+//observe Each time a file css is processed
+gulp.task('styles:watch',function(){
+	gulp.watch(paths.client.sass,['styles']);
+});
 
-gulp.task('workflow', function () {
-  gulp.src(config.sassDir)
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.sass().on('error', plugins.sass.logError))
-    .pipe(plugins.autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(plugins.cssnano())
-    .pipe(plugins.sourcemaps.write('./'))
+//task default
+gulp.task('default',['styles:watch'],function(){
 
-  .pipe(gulp.dest(config.cssDir))
 });
