2ca34e9d71f5813a1a1cbd88d635eec8fed63673
Erik Cohen <Cohenpts@gmail.com>
2016-07-28 16:09:51 -0400

    refactored code, added todo, setup jshint to lint base js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,44 @@
 'use strict';
 
+var config = require('./gulp.config')();
+
 var gulp = require('gulp'),
-	gulpLoadPlugins=require('gulp-load-plugins'),
-	plugins = gulpLoadPlugins({
+	plugins = require('gulp-load-plugins')({
 	    lazy: true,
 	    rename:{
-	    	'gulp-sass':'sass',
-	    	'gulp-autoprefixer':'autoprefixer',
 	    	'gulp-minify-css':'minifycss',
-	    	'gulp-rename':'rename',
 	    	'gulp-sass-lint':'sassLint'
 	    }
 	});
-//constants paths
-const paths = {
-    client: {
-        css:'./dist/css/',
-        sass:'./src/sass/**/*.scss',
-    },
-    config:{
-    	sass_lint:'config/sass/*.sass-lint.yml'
-    }
-}
 /**
  * Tasks
  */
 
-// The first task is to verify that no errors styles
-gulp.task('styles',['styles:lint'],function(){
-	gulp.src(paths.client.sass) 
-	.pipe(plugins.sass().on('error',plugins.sass.logError))//sass to css, if it returns a compilation error stops execution
-	.pipe(plugins.autoprefixer({ // autoprefixer
-		browsers: ['last 2 versions'],
-        cascade: false
-	}))
-	.pipe(gulp.dest(paths.client.css)) //add to folder the files minify
-	.pipe(plugins.rename({suffix: '.min'})) //rename the css adding ".min"
-    .pipe(plugins.minifycss()) //minify css
-    .pipe(gulp.dest(paths.client.css)); //add to folder the files minify 
-
-});
-
-
-//observe Each time a file css is processed
-gulp.task('styles:watch',function(){
-	gulp.watch(paths.client.sass,['styles']);
-});
-
-//in the future to add more linter style tasks
-gulp.task('styles:lint', ['styles:lint:sass'], function(){});
-
-//special task for errors in compilation of linter styles sass
-gulp.task('styles:lint:sass',function(){
-	return gulp.src(paths.client.sass)
-    .pipe(plugins.sassLint({
-      config: paths.config.sass_lint
-    }))
-    .pipe(plugins.sassLint.format())
-    .pipe(plugins.sassLint.failOnError())
-})
-
 //task default
-gulp.task('default',['styles','styles:watch'],function(){
-	
-});
-
-var config = require('./gulp.config')();
-
-// Default gulp task
-gulp.task('default', ['watch']);
+gulp.task('default',['styles','lint']);
 
 // lint and minify js
 gulp.task('lint', function() {
     return gulp.src(config.srcJS)
-        .pipe(plugins.jshint())
+        .pipe(plugins.jshint({node:true}))
         .pipe(plugins.jshint.reporter(config.jsReporter));
 });
 
 // task to compile sass, prefix and minify css
-gulp.task('styles', function(done) {
-    gulp.src(config.sassDir)
+gulp.task('styles', function() {
+    return gulp.src(config.sassDir)
         .pipe(plugins.sourcemaps.init())
-        .pipe(plugins.pipe(plugins.sass().on('error', plugins.notify(plugins.sass.logError))))
+        .pipe(plugins.sassLint())
+        .pipe(plugins.sassLint.format())
+        .pipe(plugins.sassLint.failOnError())
+        .pipe(plugins.sass().on('error', plugins.sass.logError))
         .pipe(plugins.autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(plugins.cssnano())
+        .pipe(plugins.minifycss())
         .pipe(plugins.sourcemaps.write('./'))
-
-    .pipe(gulp.dest(config.cssDir))
-        .pipe(plugins.notify({
-            message: 'Styles task complete'
-        }));
-
-    done();
-
+        .pipe(gulp.dest(config.cssDir));
 });
 
 // Watch JS for changes
@@ -103,8 +46,3 @@ gulp.task('watch', function() {
     gulp.watch(config.srcJS, ['lint']);
     gulp.watch(config.sassDir, ['styles']);
 });
-
-function exceptionLog(error) {
-    console.log(error.toString());
-    this.emit('end');
-}
---
+++ todo.md
@@ -0,0 +1,5 @@
+WeDevJS Todo
+
+# Fix the sass linter options in the gulp file
+# Implement Karma for Frontend testing
+# Implement Mocha with Chai for javascript assertion tests 
