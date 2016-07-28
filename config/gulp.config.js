093521ef474e7bf93516296ee6496c62f16e6cdc
Erik Cohen <Cohenpts@gmail.com>
2016-07-28 18:12:05 -0400

    Added live reload and watches for files and thier tasks, mocha is running with chai in the test folder

--- config/gulp.config.js
+++ config/gulp.config.js
@@ -2,7 +2,9 @@ module.exports = function() {
     var config = {
         sassDir: './src/sass/**/*.scss',
         cssDir: './dist/css/',
-        srcJS: ['./src/js/**/*.js','./*.js'],
+        srcJS: './src/js/**/*.js',
+        baseJS: './',
+        jsDir: './dist/js/',
         jsReporter: 'jshint-stylish'
     };
     
--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,78 @@
 'use strict';
 
-var config = require('./gulp.config')();
+var config = require('./config/gulp.config')();
 
 var gulp = require('gulp'),
 	plugins = require('gulp-load-plugins')({
 	    lazy: true,
 	    rename:{
 	    	'gulp-minify-css':'minifycss',
-	    	'gulp-sass-lint':'sassLint'
+	    	'gulp-sass-lint':'sassLint',
 	    }
 	});
+
+var browserSync = require('browser-sync').create();
+
 /**
  * Tasks
  */
 
+
 //task default
-gulp.task('default',['styles','lint']);
+gulp.task('default',['jade','scripts','test','styles','browser-sync']);
+
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
+
+    gulp.watch("./src/sass/*.scss", ['styles','reload']);
+    gulp.watch("./src/js/*.js", ['scripts','test','reload']);
+    gulp.watch("./src/*.jade",['jade','reload']);
+});
+
+// Browser Sync wrapper task 
+// allows for proper injection of css files
+gulp.task('reload', function(cb) {
+    browserSync.reload();
+    cb();
+});
+
+// jade compile to HTML
+gulp.task('jade', function() {
+  var YOUR_LOCALS = {};
+ 
+  gulp.src('./src/*.jade')
+    .pipe(plugins.jade({
+      locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest('./dist/'))
+});
+
+// testing
+gulp.task('test',function(){
+    return gulp.src('test/test.js', {read: false})
+        .once('error', function() {
+            process.exit(1);
+        })
+        .pipe(plugins.mocha({reporter: 'progress'}));
+});
 
 // lint and minify js
-gulp.task('lint', function() {
+gulp.task('scripts', function() {
+    gulp.src(config.baseJS)
+        .pipe(plugins.jshint({node:true}))  
+        .pipe(plugins.jshint.reporter(config.jsReporter));
+
     return gulp.src(config.srcJS)
         .pipe(plugins.jshint({node:true}))
-        .pipe(plugins.jshint.reporter(config.jsReporter));
+        .pipe(plugins.jshint.reporter(config.jsReporter))
+        .pipe(gulp.dest(config.jsDir))
+        
 });
 
 // task to compile sass, prefix and minify css
@@ -41,8 +92,10 @@ gulp.task('styles', function() {
         .pipe(gulp.dest(config.cssDir));
 });
 
+// Testing framework
+
 // Watch JS for changes
 gulp.task('watch', function() {
-    gulp.watch(config.srcJS, ['lint']);
+    gulp.watch(config.srcJS, ['scripts']);
     gulp.watch(config.sassDir, ['styles']);
 });
