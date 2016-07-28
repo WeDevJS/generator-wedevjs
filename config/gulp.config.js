96a8fe5ea73ae7b1051e5afa04da357407990230
Erik Cohen <Cohenpts@gmail.com>
2016-07-28 19:25:37 -0400

    gulp config changes

---
+++ config/gulp.config.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+    var config = {
+        sassDir: './src/sass/**/*.scss',
+        cssDir: './dist/css/',
+        srcJS: './src/js/**/*.js',
+        baseJS: './',
+        jsDir: './dist/js/',
+        jsReporter: 'jshint-stylish'
+    };
+    
+    return config;
+};
