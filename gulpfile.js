4246ffe84b96ae45ac9d0985b2e09337f43c9567
Yording <yording9@gmail.com>
2016-07-31 23:22:23 -0500

    refactor of folders,add task inject for bower,js and css

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,15 @@ var gulp = require('gulp'),
 	});
 
 var browserSync = require('browser-sync').create();
+var wiredep = require("wiredep").stream;
+
 
 /**
  * Tasks
  */
 
 //task default
-gulp.task('default',['jade','scripts','test','styles','nodemon','browser-sync']);
+gulp.task('default',['jade','scripts','test','styles','inject','wiredep','nodemon','browser-sync']);
 
 
 // Nodemon task
@@ -47,8 +49,10 @@ gulp.task('browser-sync', function() {
     });
 
     gulp.watch(config.client.sass, ['styles','reload']);
-    gulp.watch(config.client.js, ['scripts','test','reload']);
-    gulp.watch(config.client.jade,['jade','reload']);
+    gulp.watch(config.client.js, ['scripts','inject','test','reload']);
+    gulp.watch(config.client.css,['inject']);
+    gulp.watch(config.client.views,['jade','reload']);
+    gulp.watch("./bower.json", ["wiredep"]);
 });
 
 // Browser Sync wrapper task 
@@ -61,13 +65,31 @@ gulp.task('reload',function(){
 gulp.task('jade', function() {
   var YOUR_LOCALS = {};
  
-  gulp.src(config.client.jade)
+  gulp.src(config.client.views)
     .pipe(plugins.jade({
       locals: YOUR_LOCALS
     }))
-    .pipe(gulp.dest(config.dist.base))
+    .pipe(gulp.dest(config.client.base))
 });
 
+gulp.task("inject", ["wiredep"] ,function () {
+    var sources = gulp.src([config.client.js, config.client.css], {read: false});
+
+    return gulp.src(config.client.base+"index.html")
+    .pipe(plugins.inject(sources))
+    .pipe(gulp.dest(config.client.base));
+});
+gulp.task("wiredep", function () {
+    return gulp.src(config.client.base+"index.html")
+    .pipe(wiredep({
+        directory: "./bower_components",
+        read: false,
+        onError: function (err) {
+            console.log(err.code);
+        }
+    }))
+    .pipe(gulp.dest(config.client.base));
+});
 // Backend testing
 gulp.task('test',function(){
     return gulp.src(config.test.testConfig, {read: false})
